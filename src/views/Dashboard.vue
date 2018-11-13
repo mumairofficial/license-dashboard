a3a8407e10d4cab15575545cf90106fa4ecfa3df
mumair <mumairofficial@gmail.com>
2018-11-13 07:18:24 +0500

    dashboard tweaks

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,24 +2,33 @@
   <content-wrapper>
     
     <div class="flex">
-      <div class="bg-white text-grey-darkest flex-1 py-6 px-4 rounded-lg mr-4">
+      <div class="bg-white text-grey-darkest flex-1 p-4 rounded-lg mr-4">
         <div class="uppercase tracking-wide">Total License</div>
-        <div class="mt-4 text-4xl text-right">123,456,10</div>
+        <div class="mt-8 text-4xl text-right">123,456,10</div>
       </div>
       
-      <div class="bg-green-light text-white flex-1 py-6 px-4 rounded-lg mr-4">
+      <div class="bg-green-light text-white flex-1 p-4 rounded-lg mr-4">
         <div class="uppercase tracking-wide">Active</div>
-        <div class="mt-4 text-4xl text-right">123,45</div>
+        <div class="mt-8 text-4xl text-right">123,45</div>
       </div>
 
-      <div class="bg-orange-light text-white flex-1 py-6 px-4 rounded-lg mr-4">
+      <div class="bg-orange-light text-white flex-1 p-4 rounded-lg mr-4">
         <div class="uppercase tracking-wide">Expired</div>
-        <div class="mt-4 text-4xl text-right">123</div>
+        <div class="mt-8 text-4xl text-right">123</div>
       </div>
 
-      <div class="bg-blue-light text-white flex-1 py-6 px-4 rounded-lg">
-        <div class="uppercase tracking-wide">Recalled</div>
-        <div class="mt-4 text-4xl text-right">123,456,10</div>
+      <div class="bg-blue-light text-white flex-1 p-4 rounded-lg">
+        <div class="uppercase tracking-wide">Earning</div>
+        <div class="mt-8 text-4xl text-right">PKR 123,45</div>
+      </div>
+    </div>
+
+    <div class="flex mt-10">
+      <div class="flex-1 p-2 bg-white rounded-lg mr-4">
+        Recent Sale
+      </div>
+      <div class="flex-1 p-2 bg-white rounded-lg">
+        Up Coming Expiries
       </div>
     </div>
 
--- src/views/LicenseManage.vue
+++ src/views/LicenseManage.vue
@@ -1,6 +1,60 @@
 <template>
   <content-wrapper>
-    <h3>This is an license manage page</h3>
+    <div class="border border-grey-light rounded bg-white">
+      <!-- <div class="bg-grey-lighter"> -->
+        <input class="px-6 py-4 bg-grey-light w-full text-mono text-grey-darker no-outline" type="text" placeholder="Type to search..." />
+      <!-- </div> -->
+      <table class="text-left w-full" style="border-collapse:collapse">
+        <thead>
+          <tr class="">
+            <th class="py-4 px-6 bg-grey-lighter font-sans font-medium uppercase text-sm text-grey border-b border-grey-light">Issue To</th>
+            <th class="py-4 px-6 bg-grey-lighter font-sans font-medium uppercase text-sm text-grey border-b border-grey-light">Application</th>
+            <th class="py-4 px-6 bg-grey-lighter font-sans font-medium uppercase text-sm text-grey border-b border-grey-light">License Key</th>
+            <th class="py-4 px-6 bg-grey-lighter font-sans font-medium uppercase text-sm text-grey border-b border-grey-light">Expiry</th>
+            <th class="py-4 px-6 bg-grey-lighter font-sans font-medium uppercase text-sm text-grey border-b border-grey-light">Issued On</th>
+            <th class="py-4 px-6 bg-grey-lighter font-sans font-medium uppercase text-sm text-grey border-b border-grey-light">Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr class="hover:bg-blue-lightest font-mono text-sm tracking-wide">
+            <td class="py-4 px-6 border-b border-grey-light">user@email.com</td>
+            <td class="py-4 px-6 border-b border-grey-light">Remote Desktop</td>
+            <td class="py-4 px-6 border-b border-grey-light">
+              <span class="mr-2">ASDF234567812345678LJK868</span>
+              <button class="border px-3 py-1 rounded bg-grey-lighter text-sm">copy</button>
+            </td>
+            <td class="py-4 px-6 border-b border-grey-light">12-Aug-2019</td>
+            <td class="py-4 px-6 border-b border-grey-light">12-Aug-2018</td>
+            <td class="py-4 px-6 border-b border-grey-light">Active</td>
+          </tr>
+
+          <tr class="hover:bg-blue-lightest font-mono text-sm tracking-wide">
+            <td class="py-4 px-6 border-b border-grey-light">user@email.com</td>
+            <td class="py-4 px-6 border-b border-grey-light">Remote Desktop</td>
+            <td class="py-4 px-6 border-b border-grey-light">
+              <span class="mr-2">ASDF234567812345678LJK868</span>
+              <button class="border px-3 py-1 rounded bg-grey-lighter text-sm">copy</button>
+            </td>
+            <td class="py-4 px-6 border-b border-grey-light">12-Aug-2019</td>
+            <td class="py-4 px-6 border-b border-grey-light">12-Aug-2018</td>
+            <td class="py-4 px-6 border-b border-grey-light">Active</td>
+          </tr>
+
+          <tr class="hover:bg-blue-lightest font-mono text-sm tracking-wide">
+            <td class="py-4 px-6 border-b border-grey-light">user@email.com</td>
+            <td class="py-4 px-6 border-b border-grey-light">Remote Desktop</td>
+            <td class="py-4 px-6 border-b border-grey-light">
+              <span class="mr-2">ASDF234567812345678LJK868</span>
+              <button class="border px-3 py-1 rounded bg-grey-lighter text-sm">copy</button>
+            </td>
+            <td class="py-4 px-6 border-b border-grey-light">12-Aug-2019</td>
+            <td class="py-4 px-6 border-b border-grey-light">12-Aug-2018</td>
+            <td class="py-4 px-6 border-b border-grey-light">Active</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </content-wrapper>
 </template>
 
